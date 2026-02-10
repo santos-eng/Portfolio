39e45fc85dea10ec1f4d899342f66ef43ea464fa
santos-eng <techatarian@gmail.com>
2026-02-10 17:38:35 +1030

    fixing the deployment issue of the url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route, HashRouter} from 'react-router-dom';
 import Layout from './components/Layout'
 import Home from './components/Home';
 import About from './components/About';
@@ -7,15 +7,15 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home/>}/>
-        <Route path="about" element={<About/>}/>
-        <Route path="contact" element={<Contact/>}/>
-      </Route>
-    </Routes>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home/>}/>
+          <Route path="about" element={<About/>}/>
+          <Route path="contact" element={<Contact/>}/>
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 }
 
